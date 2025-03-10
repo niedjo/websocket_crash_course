fb01d756be9d1d06a556b19a61f45ae0a1dc6e8e
niedjo <niedjokuitche@gmail.com>
2025-03-10 09:23:48 +0100

    first commit

---
+++ client/index.js
@@ -0,0 +1,10 @@
+const socket = io('http://localhost:3000')
+
+socket.on('connect', (response) => {
+    console.log(response)
+})
+
+socket.on('message', (data) => {
+    console.log(data)
+    socket.emit('message', 'hello from the server and Im nkds')
+})
